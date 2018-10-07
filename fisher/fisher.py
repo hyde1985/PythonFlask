251fd465e880fe280da386b35dfd470d7b32e152
Hyde Sun <hyde_sun@sina.com>
2018-10-07 20:19:41 +0800

    code for 2-8

--- fisher/fisher.py
+++ fisher/fisher.py
@@ -1,13 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
-# @app.route('/hello')
+@app.route('/hello')
 def hello():
     return 'Hello World'
 
 
-app.add_url_rule('/hello', view_func=hello)
+# app.add_url_rule('/hello', view_func=hello)
 
-app.run(debug=True)
+app.run(debug=app.config['DEBUG'])
