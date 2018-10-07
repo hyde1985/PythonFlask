c3762801d5bdc6bf4dc828c9128014856f178ab3
Hyde Sun <hyde_sun@sina.com>
2018-10-07 21:36:56 +0800

    code for 3-3

--- fisher/fisher.py
+++ fisher/fisher.py
@@ -4,12 +4,17 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello World'
+@app.route('/book/search/<q>/<page>')
+def search(q, page):
+    # isbn13 13个0-9的数字组成
+    # isbn10 10个0-9数字组成，含有-
+    isbn_or_key = 'key'
+    if len(q) == 13 and q.isdigit():
+        isbn_or_key = 'isbn'
+    short_q = q.replace('-', '')
+    if "-" in q and len(short_q) == 10 and short_q.isdigit():
+        isbn_or_key = 'isbn'
 
-
-# app.add_url_rule('/hello', view_func=hello)
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'])
